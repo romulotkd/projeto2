1a492c7d593f19ef4c3707c1b2ed0c28bb03e4c7
BR\bdyp <romuloregis.sonda@br-petrobras.com.br>
2019-06-04 11:56:39 -0300

    1. Introdução 2. Componentes 3. Instalação - Windows 4. Instalação Linux 5. Introdução 6. Criação de um projeto 7. Componentes 8. Estilos 9. Seletores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyfirstComponent } from './myfirst.component';
+import { MysecondComponent } from './mysecond/mysecond.component';
+import { MythirdComponent } from './mythird/mythird.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyfirstComponent,
+    MysecondComponent,
+    MythirdComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mythird/mythird.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '.mythird',
+  templateUrl: './mythird.component.html',
+  styleUrls: ['./mythird.component.css']
+})
+export class MythirdComponent {
+
+}
