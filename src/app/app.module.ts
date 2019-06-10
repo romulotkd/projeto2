6cc7b8799a31132a0d1b7078abd40bfc23856171
BR\bdyp <romuloregis.sonda@br-petrobras.com.br>
2019-06-10 11:48:04 -0300

    10. Gerando um componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MyfirstComponent } from './myfirst.component';
 import { MysecondComponent } from './mysecond/mysecond.component';
-import { MythirdComponent } from './mythird/mythird.component';
+import { MythirdComponent } from './mysecond/mythird/mythird.component';
 
 @NgModule({
   declarations: [
