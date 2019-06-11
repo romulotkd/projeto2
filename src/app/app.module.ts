58cecefe47da4ff2856114447bb9e9b81580cb98
BR\bdyp <romuloregis.sonda@br-petrobras.com.br>
2019-06-11 11:29:57 -0300

    11. Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppComponent } from './app.component';
 import { MyfirstComponent } from './myfirst.component';
 import { MysecondComponent } from './mysecond/mysecond.component';
 import { MythirdComponent } from './mysecond/mythird/mythird.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MaterialComponent } from './material/material.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyfirstComponent,
     MysecondComponent,
-    MythirdComponent
+    MythirdComponent,
+    MaterialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
